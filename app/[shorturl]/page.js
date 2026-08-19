de3da93765ec93180862a838efe9215f3e880d9e
Hasnain Jaffer <hasnainjaffer65@gmail.com>
2026-08-20 04:01:58 +0500

    Refactor page.js for improved readability
    Refactor page.js to improve code readability and consistency.

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,19 +1,18 @@
-import { redirect } from "next/navigation"
-import clientPromise from "@/lib/mongodb"
+import { redirect } from "next/navigation";
+import clientPromise from "@/lib/mongodb";
 
-export default async function Page({params}) {
-  const shorturl = (await params).shorturl
+export default async function Page({ params }) {
+  const shorturl = (await params).shorturl;
+
   const client = await clientPromise;
-  const db = client.db('bitlinks')  
-  const collection = db.collection('url')  
-  
-  const doc = await collection.findOne({ shorturl: shorturl });
-if (doc) {  
-    redirect(doc.url);
-  } 
-  else {
-     redirect(`${process.env.NEXT_PUBLIC_HOST}`);
-  }
+  const db = client.db("bitlinks");
+  const collection = db.collection("url");
 
-  return <div>My Post: {shorturl}</div>
+  const doc = await collection.findOne({ shorturl });
+
+  if (doc) {
+    redirect(doc.url);
+  } else {
+    redirect("/");
+  }
 }
